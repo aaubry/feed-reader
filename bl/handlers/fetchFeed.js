0f1ca4327efb875220ee6873facf7fb5244e5da4
Antoine Aubry <aaubry@gmail.com>
2013-09-25 13:00:03 +0000

    Add support for Atom feeds

--- bl/handlers/fetchFeed.js
+++ bl/handlers/fetchFeed.js
@@ -35,10 +35,17 @@ extractors["application/rss+xml"] = function(body, cb) {
 	extract_rss(libxmljs.parseXml(body), cb);
 };
 
+extractors["application/atom+xml"] = function(body, cb) {
+	extract_atom(libxmljs.parseXml(body), cb);
+};
+
 extractors["text/xml"] = function(body, cb) {
+	var ns = { "a": "http://www.w3.org/2005/Atom" };
 	var doc = libxmljs.parseXml(body);
 	if(doc.get("/rss")) {
 		extract_rss(doc, cb);
+	} else if(doc.get("/feed", ns)) {
+                extract_atom(doc, cb);
 	} else {
 		cb("Could not parse xml");
 	}
@@ -59,4 +66,18 @@ function extract_rss(doc, cb) {
 	cb(null, items);
 }
 
+function extract_atom(doc, cb) {
+	var ns = { "a": "http://www.w3.org/2005/Atom" };
+        var nodes = doc.find("/a:feed/a:entry", ns);
 
+        var items = nodes.map(function(n) {
+
+                var result = {};
+                n.find("*").forEach(function(c) {
+                        result[c.name()] = c.text();
+                });
+                return result;
+        });
+
+        cb(null, items);
+}
