bb23d19895d1529acbebc5f35d445fcc3b26f84b
Antoine Aubry <aaubry@gmail.com>
2014-05-14 00:10:35 +0100

    Add full-text search.

--- routes/home.js
+++ routes/home.js
@@ -16,16 +16,61 @@ var config = require("../config/config");
 exports.registerRoutes = function(app, dbFactory) {
 
 	app.get("/", categories);
+	app.get("/search", search);
 	app.get("/:id", list);
 	app.get("/i/:id", view);
-
+	
 	function categories(req, res) {
 		res.render("home/categories", {
 			title: "Categories",
-			items: config.getAllCategories()
+			items: config.getAllCategories(),
+			query: req.query.q
 		});
 	}
 
+	function search(req, res) {
+
+		var db = dbFactory();
+		db.open(closeOnError(db, handleAppError(res, db_opened)));
+
+		function db_opened(conn) {
+			conn.collection("Items", closeOnError(db, handleAppError(res, items_collection_opened)));
+
+			function items_collection_opened(coll) {
+				var options = {
+					limit: 150
+				};
+
+				var fields = {
+					_id: true,
+					feedId: true,
+					title: true,
+					thumbUrl: true,
+					score: { $meta: "textScore" }
+				};
+				
+				var query = req.query.q;
+				
+				coll.find({ $text: { $search: query } }, fields, options).sort({ score: { $meta: "textScore" } }, closeOnError(db, handleAppError(res, items_sorted)));
+
+				function items_sorted(cursor) {
+					cursor.toArray(closeOnError(db, cursor, handleAppError(res, items_retrieved)));
+				}
+
+				function items_retrieved(items) {
+					res.render("home/list", {
+						title: "Feed Items",
+						items: items,
+						category: {
+							name: "Search Results"
+						},
+						query: query
+					});
+				}
+			}
+		}
+	}
+	
 	function list(req, res) {
 
 		var db = dbFactory();
@@ -61,7 +106,8 @@ exports.registerRoutes = function(app, dbFactory) {
 					res.render("home/list", {
 						title: "Feed Items",
 						items: items,
-						category: category
+						category: category,
+						query: req.query.q
 					});
 				}
 			}
@@ -99,7 +145,8 @@ exports.registerRoutes = function(app, dbFactory) {
 					var item = items[0];
 					res.render("home/view", {
 						title: item.title,
-						item: item
+						item: item,
+						query: req.query.q
 					});
 				}
 			}
