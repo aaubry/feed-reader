2ef439d57d1348e5396fca81bdd1eb4408f3a319
Antoine Aubry <aaubry@gmail.com>
2014-05-13 21:43:09 +0000

    Enable HTTPS, basic authentication and full text search.

--- app.js
+++ app.js
@@ -4,35 +4,63 @@
  */
 
 var express = require("express")
-  , http = require("http")
-  , path = require("path");
+  , https = require("https")
+  , path = require("path")
+  , fs = require("fs")
+  , bcrypt = require("bcrypt");
 
 var expressLayouts = require("express-ejs-layouts");
 
-var dbFactory = require("./bl/dbFactory").dbFactory;
+var dbF = require("./bl/dbFactory");
+var dbFactory = dbF.dbFactory;
 
-var app = express();
+dbF.initialize(database_initialized);
 
-// all environments
-app.set("port", process.env.PORT || 3000);
-app.set("views", __dirname + "/views");
-app.set("view engine", "ejs");
-app.use(expressLayouts);
-app.use(express.favicon());
-app.use(express.logger("dev"));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, "public")));
+function database_initialized(err) {
+	if(err) throw err;
 
-// development only
-if ("development" == app.get("env")) {
-  app.use(express.errorHandler());
+	var app = express();
+
+	// all environments
+	app.set("port", 443);
+	app.set("views", __dirname + "/views");
+	app.set("view engine", "ejs");
+	app.use(expressLayouts);
+	app.use(express.favicon());
+	app.use(express.logger("dev"));
+
+	var protectedPassword = "$2a$10$qermVMgGvGiiCLw3lqFGaecyuGTwmjtsl.JmCWQSKlUpFRRLQURWu";
+	var validPassword = null;
+	app.use(express.basicAuth(function(user, pass) {
+		if(user !== "brisemec") return false;
+		if(validPassword != null) {
+			return validPassword === pass;
+		}
+		
+		var result = bcrypt.compareSync(pass, protectedPassword);
+		if(result) validPassword = pass;
+		return result;
+	}));
+
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, "public")));
+
+	// development only
+	if ("development" == app.get("env")) {
+	  app.use(express.errorHandler());
+	}
+
+	//app.get("/", routes.index);
+	require("./routes/home").registerRoutes(app, dbFactory);
+
+	var options = {
+		key: fs.readFileSync("ssl/key.pem"),
+		ca: fs.readFileSync("ssl/csr.pem"),
+		cert: fs.readFileSync("ssl/cert.pem")
+	}
+	https.createServer(options, app).listen(app.get("port"), function(){
+	  console.log("Express server listening on port " + app.get("port"));
+	});
 }
-
-//app.get("/", routes.index);
-require("./routes/home").registerRoutes(app, dbFactory);
-
-http.createServer(app).listen(app.get("port"), function(){
-  console.log("Express server listening on port " + app.get("port"));
-});
--- bl/dbFactory.js
+++ bl/dbFactory.js
@@ -1,8 +1,29 @@
 var mongodb = require("mongodb"),
 	Db = mongodb.Db,
 	Server = mongodb.Server;
+
+var error = require("./error"),
+	handleAppError = error.handleAppError,
+	closeOnError = error.closeOnError;
 	
 exports.dbFactory = function() {
 	return new Db("Feeds", new Server("localhost", 27017), { w: 1 });
 };
 
+exports.initialize = function(cb) {
+	var db = exports.dbFactory();
+	
+	db.open(closeOnError(db, db_opened));
+	
+	function db_opened(err, conn) {
+		if(err) return cb(err);
+	
+		conn.collection("Items", closeOnError(db, items_collection_opened));
+
+		function items_collection_opened(err, coll) {
+			if(err) return cb(err);
+			
+			coll.ensureIndex({ title: "text", body: "text" }, cb);
+		}
+	}
+};
