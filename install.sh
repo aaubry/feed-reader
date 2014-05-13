d8fa39a70ef75b69809afe583afec698ed4a1ae5
Antoine Aubry <aaubry@gmail.com>
2014-05-14 00:10:49 +0100

    Add installation script.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+
+sudo apt-get install mongodb-org tidy phantomjs 
+
+sudo setcap cap_net_bind_service=+ep /usr/local/bin/node
